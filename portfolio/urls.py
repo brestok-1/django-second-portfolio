fb87564438b9a40e211c2042049babb8cde4f79f
brestok-1 <6234513@gmail.com>
2023-01-04 15:39:05 +0300

    before deploy

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -5,7 +5,10 @@ from .views import *
 
 urlpatterns = [
     path('', home, name='home'),  # if we want o cache defined page
-    path('about/', cache_page(60)(About.as_view()), name='about'),
+    path('about/', About.as_view(), name='about'),
     path('portfolio/', MyPortfolio.as_view(), name='portfolio'),
     path('portfolio/<slug:project_slug>', ShowProject.as_view(), name='project'),
 ]
+
+
+
