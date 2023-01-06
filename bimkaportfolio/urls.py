c942caa79d4940be2b76ba99aeed9e8ae952a949
brestok-1 <6234513@gmail.com>
2023-01-06 15:30:30 +0300

    preparing to deploy

--- bimkaportfolio/urls.py
+++ bimkaportfolio/urls.py
@@ -15,12 +15,15 @@ Including another URLconf
 """
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 from bimkaportfolio import settings
 
+from django.views.static import serve as mediaserve
+
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('adminmyportfolio/', admin.site.urls),
     path('', include('portfolio.urls')),
 ]
 
@@ -31,3 +34,10 @@ if settings.DEBUG:
     #     path('__debug__/', include('debug_toolbar.urls')),
     # ] + urlpatterns
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += [
+        re_path(f'^{settings.MEDIA_URL.lstrip("/")}(?P<path>.*)$',
+            mediaserve, {'document_root': settings.MEDIA_ROOT}),
+        re_path(f'^{settings.STATIC_URL.lstrip("/")}(?P<path>.*)$',
+            mediaserve, {'document_root': settings.STATIC_ROOT}),
+    ]
